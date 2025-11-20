c4949201c26b47d35aca41b891a8bde7882e6179
Gabriel Djemal <gabrieljoseph.djemal@post.runi.ac.ilgit config --list>
2025-11-20 10:52:53 +0200

    Completed assignment

--- Primes.java
+++ Primes.java
@@ -1,6 +1,7 @@
 public class Primes {
     public static void main(String[] args) {
         int n = Integer.parseInt(args[0]);
+        System.out.println("Prime numbers up to " + n + ":");
         boolean[] arr = new boolean[n+1];
         for (int i=2; i<=n; i++) {
             arr[i] = true;
